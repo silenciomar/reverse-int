91a1c76e24e61c9ed9755596919af7f5eca06b3f
silenciomar <96017754+silenciomar@users.noreply.github.com>
2023-01-11 09:20:24 +0600

    add reverse int solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  n = Math.abs(n);
+  n = n.toString().split('').reverse().join('')
+  if(n.endsWith("0")) {
+    n.substring(1, n.length)
+  }
+  return Number(n)
 }
